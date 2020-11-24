bd8639d28e58d3e2954f5d94d034047d9f2c1a46
Vladislav Grinin <Grinin.vlad1@gmail.com>
2020-11-24 23:24:06 +0300

    add avatar to navbar

--- src/front/src/app/shared/navbar/navbar.component.ts
+++ src/front/src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { IUser } from 'src/app/core/services/user/user.model';
 import { UserService } from 'src/app/core/services/user/user.service';
 
 @Component({
@@ -11,12 +12,14 @@ import { UserService } from 'src/app/core/services/user/user.service';
 })
 export class NavBarComponent implements OnInit {
     public isUserAuthenticated$: Observable<boolean>;
+    public user$: Observable<IUser | null>;
 
     constructor(
         private readonly userService: UserService,
         private readonly router: Router
     ) {
         this.isUserAuthenticated$ = this.userService.isAuthenticated$;
+        this.user$ = this.userService.currentUser$;
     }
 
     ngOnInit(): void {
