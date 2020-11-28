d2b0de7450637fa67c97e87a6110d389d4f21e9c
Vladislav Grinin <Grinin.vlad1@gmail.com>
2020-11-29 01:45:05 +0300

    fix members' page layout

--- src/front/src/app/members/member-list/member-list.component.ts
+++ src/front/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
 import { MemberSummary } from './member-list.model';
 import { MemberListService } from './services/member-list.service';
@@ -11,16 +12,23 @@ import { MemberListService } from './services/member-list.service';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MemberListComponent implements OnInit {
-    public members$: Observable<MemberSummary[]>;
+    public readonly defaultPageSize = 12;
 
-    private readonly pageSize = 12;
+    public members$: Observable<MemberSummary[]>;
+    public totalMemberCount$: Observable<number>;
+
+    public pageSizes = [this.defaultPageSize, 18];
 
     constructor(private readonly memberListService: MemberListService) {
         this.members$ = this.memberListService.getMembers();
+        this.totalMemberCount$ = this.memberListService.getTotalCount();
     }
 
-    ngOnInit(): void {
-        this.memberListService.loadMembers(0, this.pageSize);
+    public ngOnInit(): void {
+        this.memberListService.loadMembers(0, this.defaultPageSize);
     }
 
+    public onPageChange(event: PageEvent) {
+        this.memberListService.loadMembers(event.pageIndex, event.pageSize);
+    }
 }
